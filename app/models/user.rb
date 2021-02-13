552c3b1c29665f14006e86ffc61f6b33b1b2e66a
Akshaykumar Chakkarkota <akshay.chakkarkota@cuelinks.com>
2021-02-14 00:55:10 +0530

    Initial Commit

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+
+  has_secure_password
+  validates :username, presence: true, uniqueness: true, length: { maximum: 20, minimum: 3 }
+  validates :full_name, presence: true, length: { maximum: 50, minimum: 3 }
+  validates :password, length: { minimum: 6 }, presence: true
+end
