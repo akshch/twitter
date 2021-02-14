e439586808ec18e5df392e2d7f2f82ae21ab4da4
Akshaykumar Chakkarkota <akshay.chakkarkota@cuelinks.com>
2021-02-15 02:05:47 +0530

    Comment changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
 
+  before_action :user_login
+
   def create
     @tweet = Tweet.find_by(id: params[:id])
     @comment = @tweet.comments.new(comment_params)
@@ -17,6 +19,10 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def user_login
+    redirect_to login_path if current_user.nil?
   end
 
 end
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,21 @@
 module CommentsHelper
+  def date(comment)
+    comment.created_at.strftime('%d %h, %Y')
+  end
+
+  def comment_btn_feed(tweet)
+    if current_user.present? && tweet.user != current_user && tweet.comments.blank?
+      link_to tweet, class: "level-item" do
+  			raw("<span class='icon'><i class='far fa-comment' aria-hidden='true' ></i></span>")
+  	  end
+    elsif current_user.present? && tweet.comments.present?
+      link_to tweet, class: "level-item" do
+  		  raw("<span class='icon'><i class='fas fa-comment' aria-hidden='true' ></i></span>")
+  		end
+    else
+      link_to login_path do
+        raw("<span class='icon'><i class='far fa-comment' aria-hidden='true' ></i></span>")
+        end 
+    end
+  end
 end
