065093de8d4b5200716c68d8a8f51fbf38dd3bbf
Akshaykumar Chakkarkota <akshay.chakkarkota@cuelinks.com>
2021-02-15 11:49:10 +0530

    UI fixes

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -5,11 +5,11 @@ module CommentsHelper
 
   def comment_btn_feed(tweet)
     if current_user.present? && tweet.comments.blank?
-      link_to tweet, class: "level-item" do
+      link_to tweet do
   			raw("<span class='icon'><i class='far fa-comment' aria-hidden='true' ></i></span>")
   	  end
     elsif current_user.present? && tweet.comments.present?
-      link_to tweet, class: "level-item" do
+      link_to tweet do
   		  raw("<span class='icon'><i class='fas fa-comment' aria-hidden='true' ></i></span>")
   		end
     else
